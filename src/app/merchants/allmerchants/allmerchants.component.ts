664a4b5bb3b64343a1780f7ffaf2fd4fc09975c8
iyiolakeni <iyiolakeni@gmail.com>
2024-05-15 02:25:15 +0100

    Fixed type issue

--- src/app/merchants/allmerchants/allmerchants.component.ts
+++ src/app/merchants/allmerchants/allmerchants.component.ts
@@ -1,6 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 
+interface Merchant{
+  MerchantID: string,
+  Merchant_Trade_Name: string,
+  Business_type: string,
+  Business_location: string,
+  RC_Number: number,
+  No_of_branches: number,
+  opening_hours: string,
+  website: string,
+  Office_address: string,
+  LGA: string,
+  state: string,
+  Name_of_Primary_Contact: string,
+  office_No: string,
+  Mobile_No1: string,
+  office_email: string,
+  Designation: string,
+  Name_of_Secondary_Contact: string,
+  Designation2: string,
+  office_No2: string,
+  Mobile_No2: string,
+  office_email2: string;
+}
+
 @Component({
   selector: 'app-allmerchants',
   templateUrl: './allmerchants.component.html',
@@ -9,7 +33,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AllmerchantsComponent implements OnInit {
   @Input() n: number = 15;
   currentPage = 1;
-  merchants: any;
+  merchants: Merchant[] = [];
   totalPages: number = 30;
 
   constructor(
@@ -17,8 +41,9 @@ export class AllmerchantsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.http.get<any[]>('https://bmp-node.onrender.com/merchant/allMerchants').subscribe(response => {
-      this.merchants = response;
+    this.http.get('https://bmp-node.onrender.com/merchant/allMerchants').
+    subscribe((response: any) => {
+      this.merchants = response as Merchant[];
       console.log(this.merchants)
     },
     error => {
