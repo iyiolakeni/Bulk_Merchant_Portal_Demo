5063ec1ea3bfe0fc93e7c6f842f1e9382cb8c2e1
iyiolakeni <iyiolakeni@gmail.com>
2024-05-27 14:39:36 +0100

    Updated popup

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,15 +1,22 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.css'
 })
-export class PopupComponent {
+export class PopupComponent implements OnInit {
 
   constructor(
     private popupRef: MatDialogRef<PopupComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private sharedService: AppService
   ){}
+// const user: any;
+
+  ngOnInit(): void {
+    console.log(this.data.user)
+
 }
