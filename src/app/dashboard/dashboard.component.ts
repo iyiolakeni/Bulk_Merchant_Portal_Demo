2cbb299e609971d5b938bb4d9e9395ec029bc031
iyiolakeni <iyiolakeni@gmail.com>
2024-05-27 13:22:26 +0100

    Type oriented using Set

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
 import { ApiDetailsService } from '../api-details.service';
 
+
+interface Item{
+  status: string;
+  [key: string]: any;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -21,20 +26,22 @@ ngOnInit() {
     console.log("Response is:",response);
   })
 
-  this.apiService.getRequest().subscribe(data => { 
+  this.apiService.getRequest().subscribe((data: Item[]) => { 
     // Retrieve all status from the data set
-    const counts = new Map();
+    const statuses: string [] = [...new Set(data.map(item => item.status))];
+
+    // Initialize counts for all statuses
+    const counts: {[key: string]: number} = statuses.reduce((obj, status) => ({ ...obj, [status]: 0 }), {});
 
     // count the status in the data
     data.forEach(
-      (item: any) =>{
-        counts.set(item.status, (counts.get(item.status) || 0)+1);
+      (item: Item) =>{
+        counts[item.status];
         console.log(item.status)
       }
     );
 
     // set the dashboard to count
-    this.dashboard = Array.from(counts.entries()).map(([status, count])=>  ({status,count})).sort((a,b) => b.count - a.count);
     // const counts: { [status: string]: number } = data.reduce((counts: { [x: string]: number; }, item: { status: string | number; }) => { 
     //   // Use reduce to count the statuses
     //   if (!counts[item.status]) {
@@ -44,7 +51,7 @@ ngOnInit() {
     //   return counts;
     // }, {});
 
-    // this.dashboard = Object.entries(counts).map(([status, count]) => ({ status, count: Number(count) })).sort((a, b) => b.count - a.count);
+    this.dashboard = Object.entries(counts).map(([status, count]) => ({ status, count: Number(count) })).sort((a, b) => b.count - a.count);
 
     //Total Request count
     this.totalRequest = this.dashboard.reduce((total, item) => total + item.count, 0);
